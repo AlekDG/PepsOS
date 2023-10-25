70941285c07077da6d1bd94843f4c155d5043f54
nicanorPorto <119369734+nicanorPorto@users.noreply.github.com>
2023-10-24 21:27:52 -0300

    Update buildSnakeMap.c

--- Kernel/buildSnakeMap.c
+++ Kernel/buildSnakeMap.c
@@ -1,30 +1,13 @@
 #include <buildSnakeMap.h>
 #include <stdint.h>
+#include "video.h"
 
 // faltaria ver como hacer para que el mapa ocupe toda la pantalla
 
 void buildMap()
 {
-      int mapSize = 20;
       int squareSize = 40;
-      uint32_t green = 0x15F03;
-      uint32_t lightGreen = 0xC0E3AF;
+      uint32_t caramelBrown = 0x613613;
 
-      for (int i = 0; i < mapSize; i++)
-      {
-            for (int j = 0; j < mapSize; j++)
-            {
-                  int x = i * squareSize;
-                  int y = j * squareSize;
-
-                  if ((i + j) % 2 != 0)
-                  {
-                        drawRectangle(green, x, y, squareSize, squareSize);
-                  }
-                  else
-                  {
-                        drawRectangle(lightGreen, x, y, squareSize, squareSize);
-                  }
-            }
-      }
+      drawRectangle(caramelBrown, 0, 0, getFullWidth(), getFullHeight());
 }
