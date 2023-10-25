3d3050f7c0b51a38f2d5d475737acc655904ed9d
nicanorPorto <119369734+nicanorPorto@users.noreply.github.com>
2023-10-24 21:27:32 -0300

    Update snake.c

--- Kernel/snake.c
+++ Kernel/snake.c
@@ -2,21 +2,35 @@
 #include <stdint.h>
 #include <clock.h>
 #include <time.h>
+#include "video.h"
+
+#define SQUARESIZE 40
+#define DELAYTICKS 6 // Es aproximadamente 0.3 segs
+
+enum Movement
+{
+      DOWN,
+      UP,
+      RIGHT,
+      LEFT
+};
 
 struct Point
 {
-      uint8_t x;
-      uint8_t y;
+      uint32_t x;
+      uint32_t y;
 };
 
 struct Snake
 {
       struct Point body[20]; // Habria q ver cual seria la mayor cantidad de partecitas para la vibora
       // body[0] seria la "cabeza", que es lo que marca el recorrido
-      uint8_t length;
+      uint32_t length;
 };
 
 struct Snake snake;
+
+enum Movement mov = DOWN;
 
 // Habria que hacer un menu inicial para que cuando el jugador pierda pueda volver a jugar apretando ahi
 
@@ -32,22 +46,21 @@ void start_game()
 
       int lastTick = ticks_elapsed();
       int currentTick;
-      unsigned int delay = 6;
+      unsigned int delay = DELAYTICKS;
 
-      int mapWidth = 20 * 40;
-      int mapHeight = 20 * 40;
+      int mapWidth = getFullWidth();
+      int mapHeight = getFullHeight();
 
       uint32_t white = 0xFFFFFF;
-      uint32_t green = 0x15F03;
-      uint32_t lightGreen = 0xC0E3AF;
+      uint32_t caramelBrown = 0x613613;
 
       while (flagWall == 0 && flagSnake == 0)
       {
             currentTick = ticks_elapsed();
 
             // Chequeamos si se choca con la pared
-            if (snake.body[0].x >= mapWidth || snake.body[0].x < 0 ||
-                snake.body[0].y >= mapHeight || snake.body[0].y < 0)
+            if (snake.body[snake.length - 1].x >= mapWidth || snake.body[snake.length - 1].x < 0 ||
+                snake.body[snake.length - 1].y >= mapHeight || snake.body[snake.length - 1].y < 0)
             {
                   flagWall = 1;
             }
@@ -68,26 +81,62 @@ void start_game()
 
                   // Este seria el codigo para el caso en el que se toque la tecla de abajo:
 
-                  uint16_t lastTailX = snake.body[0].x;
-                  uint16_t lastTailY = snake.body[0].y;
+                  uint32_t lastTailX = snake.body[0].x;
+                  uint32_t lastTailY = snake.body[0].y;
 
                   for (int i = 0; i < snake.length - 1; i++)
                   {
                         snake.body[i] = snake.body[i + 1];
+                        drawRectangle(white, snake.body[i].x, snake.body[i].y, SQUARESIZE, SQUARESIZE);
                   }
-                  snake.body[snake.length - 1].y += 40;
 
-                  drawRectangle(white, snake.body[snake.length - 1].x, snake.body[snake.length - 1].y, 40, 40); // Pintamos la "cabeza nueva"
+                  switch (mov)
+                  {
+                  case DOWN:
+                        snake.body[snake.length - 1].y += SQUARESIZE;
+                        break;
+                  case UP:
+                        snake.body[snake.length - 1].y -= SQUARESIZE;
+                        break;
+                  case RIGHT:
+                        snake.body[snake.length - 1].x += SQUARESIZE;
+                        break;
+                  case LEFT:
+                        snake.body[snake.length - 1].x -= SQUARESIZE;
+                        break;
+                  }
 
-                  uint32_t boardX = lastTailX / 40; // Dejamos el mapa tal cual estaba originalmente, antes del paso de la snake x ahi
-                  uint32_t boardY = lastTailY / 40;
-                  uint32_t originalColor = ((boardX + boardY) % 2 != 0) ? green : lightGreen;
-                  drawRectangle(originalColor, lastTailX, lastTailY, 40, 40); // Changed to drawRectangle
+                  drawRectangle(white, snake.body[snake.length - 1].x, snake.body[snake.length - 1].y, SQUARESIZE, SQUARESIZE); // Pintamos la "cabeza nueva"
+
+                  drawRectangle(caramelBrown, lastTailX, lastTailY, SQUARESIZE, SQUARESIZE);
 
                   lastTick = currentTick;
             }
       }
+      if (checkFlag == 0)
+      {
+            drawLetterBuffered("A");
+      }
       // aca lanzamos un mensajito en pantalla diciendo "perdiste. comiste x caras"
+}
+
+void moveSnake(uint8_t value)
+{
+      switch (value)
+      {
+      case 0:
+            mov = DOWN;
+            break;
+      case 1:
+            mov = UP;
+            break;
+      case 2:
+            mov = RIGHT;
+            break;
+      case 3:
+            mov = LEFT;
+            break;
+      }
 }
 
 void initializeSnake(struct Snake *snake)
