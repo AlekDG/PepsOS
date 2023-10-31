1c1625927f9c5e1a723d5435f6910db61c51c454
ale <alepadumori@gmail.com>
2023-10-31 18:52:52 -0300

    Made Userland usable jajajajajajajaajaj

---
+++ Userland/SampleCodeModule/asm/userSysCalls.asm
@@ -0,0 +1,16 @@
+GLOBAL call_read
+GLOBAL call_write
+GLOBAL call_time
+GLOBAL call_regRead
+GLOBAL call_clearScrean
+GLOBAL call_drawRectangle
+GLOBAL call_drawCenteredRectangle
+GLOBAL call_drawCircle
+GLOBAL call_drawFace
+GLOBAL call_bufferPosition
+GLOBAL call_getChar
+GLOBAL call_wait
+GLOBAL call_ticks
+GLOBAL call_setTextSize
+GLOBAL call_drawBufferedChar
+GLOBAL call_beep
---
+++ Userland/SampleCodeModule/include/UserSyscalls.h
@@ -0,0 +1,27 @@
+#include <stdint.h>
+
+#ifndef USERSYSCALLS 
+#define USERSYSCALLS
+
+#define INCODE 0
+#define OUTCODE 1
+#define ERRCODE 2
+
+void call_read(char *strn,int size,int fd);
+void call_write(char *strn,int size,int fd);
+void call_time(char* strn);
+void call_regRead(void);
+void call_clearScrean(uint32_t color);
+void call_drawRectangle(uint32_t color,uint32_t offset_x,uint32_t offset_y,uint32_t height);
+void call_drawCenteredRectangle(uint32_t color,uint32_t offset_x,uint32_t offset_y,uint32_t height);
+void call_drawCircle(uint32_t color,uint32_t offset_x,uint32_t offset_y,uint32_t radius);
+void call_drawFace(uint32_t offset_x,uint32_t offset_y,uint32_t size);
+int call_bufferPosition();
+uint8_t call_getChar(uint32_t position);
+void call_wait(int ticks);
+int call_ticks(void);
+void call_setTextSize(int size);
+char call_drawBufferedChar(void);
+void call_beep(uint32_t freq,uint8_t tix);
+
+#endif
