fe9be4b7a68be700f8fb9274bb0ed964b99bdf58
nicanorPorto <119369734+nicanorPorto@users.noreply.github.com>
2023-11-01 17:10:54 -0300

    Update irqDispatcher.c

--- Kernel/irqDispatcher.c
+++ Kernel/irqDispatcher.c
@@ -128,6 +128,13 @@ int int_80(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8, 
 	case 29:
 		getYBufferPointer();
 		break;
+	case 30:
+		setFGColor(rsi);
+		break;
+	case 31:
+		setBGColor(rsi);
+		break;
+	
 	default:
 		return 0;
 	}
