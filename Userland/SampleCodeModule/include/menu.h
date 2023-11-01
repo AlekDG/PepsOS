cd454a3bdb6c66fd1881353bc209a34319277684
nicanorPorto <119369734+nicanorPorto@users.noreply.github.com>
2023-10-31 22:54:34 -0300

    Create menu.h

---
+++ Userland/SampleCodeModule/include/menu.h
@@ -0,0 +1,28 @@
+#ifndef MENU_H
+#define MENU_H
+
+#define LIGHT_GRAY 0xAAAAAA
+#define BLACK 0x000000
+
+typedef struct Border
+{
+      int thickness;
+      int length;
+      int height;
+} Border;
+
+typedef struct Option
+{
+      int isClicked;
+      int isHovered;
+      Border borde;
+      char texto[50];
+} Option;
+
+typedef struct OptionMenu
+{
+      Option *options[5];
+} OptionMenu;
+
+
+#endif
