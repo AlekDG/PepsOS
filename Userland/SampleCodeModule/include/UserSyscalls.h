8bb6fe8705239c8a3813503ae28fbffe0cd7bd35
nicanorPorto <119369734+nicanorPorto@users.noreply.github.com>
2023-10-31 21:09:06 -0300

    Update UserSyscalls.h

--- Userland/SampleCodeModule/include/UserSyscalls.h
+++ Userland/SampleCodeModule/include/UserSyscalls.h
@@ -11,7 +11,7 @@ void call_read(char *strn,int size,int fd);
 void call_write(char *strn,int size,int fd);
 void call_time(char* strn);
 void call_regRead(void);
-void call_clearScrean(uint32_t color);
+void call_paintScrean(uint32_t color);
 void call_drawRectangle(uint32_t color,uint32_t offset_x,uint32_t offset_y,uint32_t width,uint32_t height);
 void call_drawCenteredRectangle(uint32_t color,uint32_t offset_x,uint32_t offset_y,uint32_t width,uint32_t height);
 void call_drawCircle(uint32_t color,uint32_t offset_x,uint32_t offset_y,uint32_t radius);
