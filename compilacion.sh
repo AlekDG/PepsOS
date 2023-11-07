35a90468fc6f7557c39150fb512f1e5d9560cad9
ale <alepadumori@gmail.com>
2023-11-07 18:45:37 -0300

    Fixed bash scrips - Deleted elf file

--- compilacion.sh
+++ compilacion.sh
@@ -1,19 +1,5 @@
-#docker start CATEDRA
-#docker exec -it CATEDRA make clean -C/root/Toolchain
-#docker exec -it CATEDRA make clean -C/root/
-#docker exec -it CATEDRA make -C/root/Toolchain
-#docker exec -it CATEDRA make -C/root/
-#docker stop CATEDRA
-
-#una vez dentro del proyecto, en la terminal de docker:
-#cd /root
-#cd Toolchain
-#make all
-#cd ..
-#make all
-
+#!/bin/bash
 cd ./Toolchain/
 make clean all
 cd ..
 make clean all
-./run.sh
--- run.sh
+++ run.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if [[ "$1" = "gdb" ]]; then
-sudo qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -soundhw pcspk -d int
-else
-sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -soundhw pcspk -audiodev pa,id=audio0
-fi
+qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -soundhw pcspk -audiodev pa,id=audio0
