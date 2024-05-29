fdbf424f2067523e415e0a5d9e84c4623f9dca65
Blemmingan <jyulan@itba.edu.ar>
2024-05-29 11:39:07 -0300

    Added .h to BlockMemoryManager.c because I forgot to push it before because I was in a state of some confusion

---
+++ Kernel/include/BlockMemoryManager.h
@@ -0,0 +1,11 @@
+//
+// Created by jo on 17/05/24.
+//
+
+#ifndef SRC_BLOCKMEMORYMANAGER_H
+#define SRC_BLOCKMEMORYMANAGER_H
+
+typedef struct MemoryManagerCDT * MemoryManagerADT;
+typedef struct BlockCDT * BlockADT;
+
+#endif //SRC_BLOCKMEMORYMANAGER_H
