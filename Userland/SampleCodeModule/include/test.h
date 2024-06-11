ff950ae729af949da061a22b708d3ffaccaa2e8d
Tobias Pugliano <tpugliano@itba.edu.ar>
2024-06-10 21:54:25 -0300

    Add catedra scheduler tests.

---
+++ Userland/SampleCodeModule/include/test.h
@@ -0,0 +1,7 @@
+#ifndef TEST_H
+#define TEST_H
+void test();
+void test_prio();
+int64_t test_processes(uint64_t argc, char *argv[]) ;
+
+#endif
---
+++ Userland/SampleCodeModule/test.c
@@ -0,0 +1,25 @@
+#include <UserSyscalls.h>
+#include <test.h>
+
+void test(){
+    call_drawRectangle(BLACK,0,0,call_getWidth(), call_getHeight());
+    call_setXBuffer(0);
+    call_setYBuffer(0);
+    //call_drawStringFormatted("TEST PROC :\n",WHITE,BLACK,2);
+    test_processes(0,"hola");
+    //    call_setXBuffer(0);
+    //call_setYBuffer(0);
+    //call_drawStringFormatted("OK :\n",WHITE,BLACK,2);
+    call_sleep(5);
+    
+    call_drawRectangle(BLACK,0,0,call_getWidth(), call_getHeight());
+        call_setXBuffer(0);
+    call_setYBuffer(0);
+    call_drawStringFormatted("TEST PRIO :\n",WHITE,BLACK,2);
+    test_prio();
+
+    call_drawStringFormatted("FINISH CATEDRA TESTS :\n",WHITE,BLACK,2);
+    call_sleep(5);
+    call_exit();
+}
+
