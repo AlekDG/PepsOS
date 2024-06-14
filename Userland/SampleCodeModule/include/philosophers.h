bcf4df6c0ce047cd592e60193ce4c7a31ae92f49
Blemmingan <jyulan@itba.edu.ar>
2024-06-14 09:52:41 -0300

    At last!!!! By the machinations of my clone, I am free* from the shadows of frozen slumber!!
    *no

--- Userland/SampleCodeModule/include/philosophers.h
+++ Userland/SampleCodeModule/include/philosophers.h
@@ -3,6 +3,22 @@
 #include <stdint.h>
 #include <UserSyscalls.h>
 
+int my_atoi(const char *str);
+char* my_itoa(int num, char* str, int base);
+void reverse(char* str, int length);
+
+void think(int i);
+void eat(int i);
+void testPhylo(int i);
+void take_forks(int i);
+void put_forks(int i);
+void philosopher(int argc, char **argv);
+
+
+void initializePhilosophers();
+void start_philosophers();
+void render_philosophers();
 void run_Philosophers(int argc, char **argv);
+
 
 #endif
--- Userland/SampleCodeModule/philosophers.c
+++ Userland/SampleCodeModule/philosophers.c
@@ -1,6 +1,7 @@
 #include <UserSyscalls.h>
 #include <user_lib.h>
 
+// Macros
 #define MAX_PHILOS 10
 #define MIN_PHILOS 3
 #define SEM_ID "Question"
@@ -9,11 +10,9 @@
 #define THINK_TIME 2
 #define EAT_TIME 3
 
-
 #define COMMAND_ADD 'a'
 #define COMMAND_REMOVE 'r'
 #define COMMAND_QUIT 'q'
-
 
 #define left(i) (((i) + qtyPhilosophers - 1) % qtyPhilosophers)
 #define right(i) (((i) + 1) % qtyPhilosophers)
@@ -22,308 +21,236 @@
 
 typedef enum { NONE = 0, EATING, HUNGRY, THINKING } PHILOSOPHER_STATE;
 
-static uint8_t qtyPhilosophers = 0;
+// Static variables
+static uint8_t qtyPhilosophers = MIN_PHILOS;
 static PHILOSOPHER_STATE philoStates[MAX_PHILOS];
 static int16_t philoPids[MAX_PHILOS];
-static uint8_t singleLine = 0;
+static int forks[MAX_PHILOS];
+static int stateLock;
 
-static void render();
-static int philosopher(int argc, char **argv);
-static int addPhilosopher(int index, int phsemID);
-static int removePhilosopher(int index, int phsemID);
-static void takeForks(int i, int phsmID, int phID);
-static void putForks(int i, int phsmID, int phID);
-static void test(int i, int phID);
+// Philosopher names
+static char *philoNames[] = {
+        "Montesqueso",  "Fridge",        "Spinetta",
+        "Gillotina",    "Chasquibum",    "Triple",
+        "Milldeck",     "Key",           "Ancla Scantron",
+        "Descartado",   "Confundido",    "Plato",
+        "Panacea",      "Planeta Enano", "El Filosofo Que No Debe Ser Nombrado",
+        "Heidi",        "Aquiles",       "Bungee",
+        "Sastre",       "Espinosa",      "de Beaver",
+        "Penduloso",    "Leipzig",       "Growl",
+        "Calvin",       "Arkham",        "Witty",
+        "Kindergarde",  "Canto",         "Teapot",
+        "Artistofanes", "Caramel Marks"};
 
-static const char *philoNames[] = {
-    "Montesqueso",  "Fridge",        "Spinetta",
-    "Gillotina",    "Chasquibum",    "Triple",
-    "Milldeck",     "Key",           "Ancla Scantron",
-    "Descartado",   "Confundido",    "Plato",
-    "Panacea",      "Planeta Enano", "El Filosofo Que No Debe Ser Nombrado",
-    "Heidi",        "Aquiles",       "Bungee",
-    "Sastre",       "Espinosa",      "de Beaver",
-    "Penduloso",    "Leipzig",       "Growl",
-    "Calvin",       "Arkham",        "Witty",
-    "Kindergarde",  "Canto",         "Teapot",
-    "Artistofanes", "Caramel Marks"};
+// Utility functions
+int my_atoi(const char *str);
+void reverse(char *str, int length);
+char *my_itoa(int num, char *str, int base);
+
+// Philosopher functions
+void philosopher(int argc, char **argv);
+void takeForks(int i);
+void putForks(int i);
+void testPhilo(int i);
+void startPhilosopher(int index);
+void initializePhilosophers();
+void stopPhilosopher(int index);
+void renderPhilosophers();
+void run_Philosophers(int argc, char **argv);
 
 int my_atoi(const char *str) {
-  int result = 0;
-  int sign = 1;
-
-  // Skip whitespaces
-  while (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\r' ||
-         *str == '\f' || *str == '\v') {
-    str++;
-  }
-
-  // Check for sign
-  if (*str == '-') {
-    sign = -1;
-    str++;
-  } else if (*str == '+') {
-    str++;
-  }
-
-  // Convert digits to integer
-  while (*str >= '0' && *str <= '9') {
-    result = result * 10 + (*str - '0');
-    str++;
-  }
-
-  return result * sign;
+    int res = 0;
+    for (int i = 0; str[i] != '\0'; ++i)
+        res = res * 10 + str[i] - '0';
+    return res;
 }
 
 void reverse(char *str, int length) {
-  int start = 0;
-  int end = length - 1;
-  while (start < end) {
-    char temp = str[start];
-    str[start] = str[end];
-    str[end] = temp;
-    start++;
-    end--;
-  }
+    int start = 0;
+    int end = length - 1;
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
 }
 
 char *my_itoa(int num, char *str, int base) {
-  int i = 0;
-  int isNegative = 0;
+    if (str == NULL) {
+        return NULL;
+    }
+    int i = 0;
+    int isNegative = 0;
 
-  // Handle 0 explicitly, otherwise empty string is printed for 0
-  if (num == 0) {
-    str[i++] = '0';
+    if (num == 0) {
+        str[i++] = '0';
+        str[i] = '\0';
+        return str;
+    }
+
+    if (num < 0 && base == 10) {
+        isNegative = 1;
+        num = -num;
+    }
+
+    while (num != 0) {
+        int rem = num % base;
+        str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+        num = num / base;
+    }
+
+    if (isNegative)
+        str[i++] = '-';
+
     str[i] = '\0';
+    reverse(str, i);
     return str;
-  }
+}
 
-  // In standard itoa(), negative numbers are handled only with base 10
-  // Otherwise, numbers are considered unsigned
-  if (num < 0 && base == 10) {
-    isNegative = 1;
-    num = -num;
-  }
+void philosopher(int argc, char **argv) {
+    int i = my_atoi(argv[0]);
+    call_drawStringFormatted("Philosopher ", WHITE, BLACK, 2);
+    call_drawStringFormatted(philoNames[i], WHITE, BLACK, 2);
+    call_drawStringFormatted(" entered\n", WHITE, BLACK, 2);
 
-  // Process individual digits
-  while (num != 0) {
-    int rem = num % base;
-    str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
-    num = num / base;
-  }
+    philoStates[i] = THINKING;
 
-  // If number is negative, append '-'
-  if (isNegative) {
-    str[i++] = '-';
-  }
+    while (1) {
+        call_sleep(THINK_TIME);
 
-  str[i] = '\0'; // Append string terminator
+        call_sem_wait(stateLock);
+        philoStates[i] = HUNGRY;
+        testPhilo(i);
+        call_sem_post(stateLock);
 
-  // Reverse the string
-  reverse(str, i);
+        takeForks(i);
+        call_sleep(EAT_TIME);
+        putForks(i);
+    }
+}
 
-  return str;
+void startPhilosopher(int index) {
+    call_drawStringFormatted("Adding philosopher: ", WHITE, BLACK, 2);
+    call_drawStringFormatted(philoNames[index], WHITE, BLACK, 2);
+    call_drawStringFormatted("\n", WHITE, BLACK, 2);
+
+    char philoIndexStr[MAX_PHILO_BUFFER];
+    my_itoa(index, philoIndexStr, 10);
+
+    char *params[] = {philoIndexStr, NULL};
+    philoPids[index] = call_createBackgroundProcess(philosopher, 1, params, 4, 0);
+}
+
+void initializePhilosophers() {
+    stateLock = call_sem_create(1, "StateLock");
+    for (int i = 0; i < MAX_PHILOS; i++) {
+        philoStates[i] = NONE;
+        philoPids[i] = -1;
+        forks[i] = call_sem_create(1, philoNames[i]);
+    }
+
+    for (int i = 0; i < MIN_PHILOS; i++) {
+        startPhilosopher(i);
+    }
+}
+
+void stopPhilosopher(int index) {
+    call_drawStringFormatted("Removing philosopher: ", WHITE, BLACK, 2);
+    call_drawStringFormatted(philoNames[index], WHITE, BLACK, 2);
+    call_drawStringFormatted("\n", WHITE, BLACK, 2);
+
+    if (philoPids[index] != -1) {
+        call_kill(philoPids[index]);
+        philoPids[index] = -1;
+    }
 }
 
 void run_Philosophers(int argc, char **argv) {
     call_paintScreen(A_LOVELY_COLOR_FOR_DINING);
     call_begin_gameplay();
 
-    int phSemID = call_sem_create(1, SEM_ID);
-    if (phSemID == -1) {
-        call_drawStringFormatted("No se pudo crear el semaforo...\n", WHITE, BLACK, 2);
+    int semID = call_sem_create(1, SEM_ID);
+    if (semID == -1) {
+        call_drawStringFormatted("Failed to create semaphore...\n", WHITE, BLACK, 2);
         return;
     }
 
-    for (int i = 0; i < MAX_PHILOS; i++) {
-        philoStates[i] = NONE;
-        philoPids[i] = -1;
-    }
+    initializePhilosophers();
 
-    for (int i = 0; i < MIN_PHILOS; i++) {
-        if (addPhilosopher(i, phSemID) == -1) {
-            call_drawStringFormatted("No se pudieron crear los filosofos iniciales...\n", WHITE, BLACK, 2);
-        }
-        call_wait(SHORT_WAIT);
-    }
     char command = '\0';
-    while ((command = (char) call_getChar()) != COMMAND_QUIT) {
+    while ((command = (char)call_getChar()) != COMMAND_QUIT) {
         switch (command) {
             case COMMAND_ADD:
                 if (qtyPhilosophers < MAX_PHILOS) {
-                    if (addPhilosopher(qtyPhilosophers, phSemID) == -1) {
-                        call_drawStringFormatted("No se pudo agregar filosofo...\n", WHITE, BLACK, 2);
-                    }
+                    startPhilosopher(qtyPhilosophers);
+                    qtyPhilosophers++;
                 } else {
-                    call_drawStringFormatted("Mesa llena!\n", WHITE, BLACK, 2);
+                    call_drawStringFormatted("Table full!\n", WHITE, BLACK, 2);
                 }
-                call_wait(SHORT_WAIT);
+                renderPhilosophers();
                 break;
             case COMMAND_REMOVE:
                 if (qtyPhilosophers > MIN_PHILOS) {
-                    if (removePhilosopher(qtyPhilosophers - 1, phSemID) == -1) {
-                        call_drawStringFormatted("No se pudo remover filosofo...\n", WHITE, BLACK, 2);
-                    }
+                    stopPhilosopher(qtyPhilosophers - 1);
+                    qtyPhilosophers--;
                 } else {
-                    call_drawStringFormatted("Mesa muy vacia!\n", WHITE, BLACK, 2);
+                    call_drawStringFormatted("Table too empty!\n", WHITE, BLACK, 2);
                 }
-                call_wait(SHORT_WAIT);
+                renderPhilosophers();
                 break;
             default:
-
-                call_drawStringFormatted("COMANDO DESCONOCIDO E INESCRUTABLE\n", WHITE, BLACK, 2);
+                //call_drawStringFormatted("Please enter a command.\n", WHITE, BLACK, 2);
                 break;
-
         }
-       render();
+
     }
 
     for (int i = qtyPhilosophers - 1; i >= 0; i--) {
-        removePhilosopher(i, phSemID);
+        stopPhilosopher(i);
     }
     call_sem_close(SEM_ID);
+    call_sem_close("StateLock");
     call_end_gameplay();
 }
 
+void takeForks(int i) {
+    call_sem_wait(forks[i]);
+    call_sem_wait(forks[right(i)]);
+}
 
-static void render() {
-    const static char letters[] = {' ', 'E', '.', '.'};
+void putForks(int i) {
+    call_sem_post(forks[i]);
+    call_sem_post(forks[right(i)]);
+
+    call_sem_wait(stateLock);
+    philoStates[i] = THINKING;
+    testPhilo(left(i));
+    testPhilo(right(i));
+    call_sem_post(stateLock);
+}
+
+void testPhilo(int i) {
+    if (philoStates[i] == HUNGRY && philoStates[left(i)] != EATING && philoStates[right(i)] != EATING) {
+        philoStates[i] = EATING;
+        call_sem_post(forks[i]);
+    }
+}
+
+void renderPhilosophers() {
+    const static char letters[] = {'.', 'E', '.', '.'};
     int yPos = 0;
-
-
+    call_wait(10);
     call_paintScreen(A_LOVELY_COLOR_FOR_DINING);
 
     for (int i = 0; i < qtyPhilosophers; i++) {
-
         call_setXBuffer(0);
         call_setYBuffer(yPos);
-        call_drawStringFormatted(&letters[philoStates[i]], WHITE, BLACK, 2);
+        char stateChar = letters[philoStates[i]];
+        call_drawStringFormatted(&stateChar, WHITE, BLACK, 2);
         yPos += 16; // Move to the next line based on font size
         call_drawStringFormatted("  ", WHITE, BLACK, 2);
     }
 }
-
-
-
-
-static int philosopher(int argc, char **argv) {
-  int i = my_atoi(argv[1]);
-    call_drawStringFormatted("Entra el filosofo ",
-                             WHITE, BLACK, 2);
-
-    call_drawStringFormatted(philoNames[i], WHITE, BLACK, 2);
-    call_drawStringFormatted("\n", WHITE, BLACK, 2);
-
-    philoStates[i] = THINKING;
-
-    int semId = my_atoi(argv[2]);
-    int phId = my_atoi(argv[3]);
-  while (1) {
-    call_sleep(THINK_TIME);
-    takeForks(i, semId, phId);
-    call_sleep(EAT_TIME);
-    putForks(i, semId, phId);
-  }
-}
-
-static int addPhilosopher(int index, int phsemID) {
-    call_sem_wait(phsemID);
-
-    call_drawStringFormatted("Sumando filosofo: ", WHITE, BLACK, 2);
-    call_drawStringFormatted(philoNames[index], WHITE, BLACK, 2);
-    call_drawStringFormatted("\n", WHITE, BLACK, 2);
-
-    char philoNumberBuffer[MAX_PHILO_BUFFER] = {0};
-    char * semIdStr = {0};
-    semIdStr = my_itoa(phsemID, semIdStr, 10);
-    int phId = call_sem_create(1, philoNames[index]);
-
-    if (phId == -1) {
-        call_drawStringFormatted("No se pudo crear el semaforo...\n", WHITE, BLACK, 2);
-        call_sem_post(phsemID);
-        return -1;
-    }
-    char  * phIdStr = {0};
-    phIdStr = my_itoa(phId, phIdStr, 10);
-    my_itoa(index, philoNumberBuffer, 10);
-    char *params[] = {"philosopher", philoNumberBuffer, semIdStr, phIdStr, NULL};
-    philoPids[index] = call_createBackgroundProcess(philosopher, 4, params, 4,0);
-    call_wait(320);
-    if (philoPids[index] != -1) {
-        qtyPhilosophers++;
-        call_sem_post(phsemID);
-        return 0;
-    } else {
-        call_drawStringFormatted("No se pudo crear el proceso filosofo...\n", WHITE, BLACK, 2);
-        call_sem_close(philoNames[index]);
-        call_sem_post(phsemID);
-        return -1;
-    }
-}
-
-
-static void takeForks(int i, int phsemID, int phID) {
-    call_drawStringFormatted("test\n", WHITE, BLACK, 2);
-    call_printInteger(call_get_sem_by_name("Montesqueso"));
-    call_sem_wait(phsemID);
-    philoStates[i] = HUNGRY;
-    test(i, phID);
-    call_sem_post(phsemID);
-    call_sem_wait(phID);
-}
-
-static void putForks(int i, int phsemID, int phID) {
-    call_sem_wait(phsemID);
-    philoStates[i] = THINKING;
-    render();
-    test(left(i), phID);
-    test(right(i), phID);
-    call_sem_post(phsemID);
-}
-
-static void test(int i, int phID) {
-    if (philoStates[i] == HUNGRY && philoStates[left(i)] != EATING && philoStates[right(i)] != EATING) {
-        philoStates[i] = EATING;
-        render();
-        call_sem_post(phID);
-    }
-}
-
-static int removePhilosopher(int index, int phsemID) {
-    call_sem_wait(phsemID);
-
-    if (philoPids[index] == -1) {
-        call_drawStringFormatted("No existe el filosofo!\n", WHITE, BLACK, 2);
-        call_sem_post(phsemID);
-        return -1;
-    }
-
-    call_drawStringFormatted("Removiendo: ", WHITE, BLACK, 2);
-    call_drawStringFormatted(philoNames[index], WHITE, BLACK, 2);
-    call_drawStringFormatted("\n", WHITE, BLACK, 2);
-
-    if (call_kill(philoPids[index]) == -1) {
-        call_drawStringFormatted("El filosofo se rehusa a morir.\n", WHITE, BLACK, 2);
-        call_sem_post(phsemID);
-        return -1;
-    }
-
-    call_waitKids();
-    philoPids[index] = -1;
-    philoStates[index] = NONE;
-    qtyPhilosophers--;
-
-    if (call_sem_close(philoNames[index]) == -1) {
-        call_drawStringFormatted("No se pudo cerrar el semaforo...", WHITE, BLACK, 2);
-        call_drawStringFormatted(philoNames[index], WHITE, BLACK, 2);
-        call_drawStringFormatted("\n", WHITE, BLACK, 2);
-    } else {
-        call_drawStringFormatted("Cerrado el semaforo de: ", WHITE, BLACK, 2);
-        call_drawStringFormatted(philoNames[index], WHITE, BLACK, 2);
-        call_drawStringFormatted("\n", WHITE, BLACK, 2);
-    }
-    call_sem_post(phsemID);
-    return 0;
-}
-
 
