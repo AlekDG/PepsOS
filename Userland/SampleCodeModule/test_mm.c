cbdf21162c0e28ff489b9638d89e168de02b704c
Blemmingan <jyulan@itba.edu.ar>
2024-06-15 15:26:21 -0300

    testmm now receives by argument and AAAAAAAAAAAAARRRRRRFHTTHEPINUPAORVUUIRVN

--- Userland/SampleCodeModule/test_mm.c
+++ Userland/SampleCodeModule/test_mm.c
@@ -5,7 +5,8 @@
 
 #define MAX_BLOCKS 128
 #define A_USEFUL_COEFFICIENT 10000
-
+#define MEGABYTES_COEFFICIENT 1048576
+#define MINIMUM_MEMORY 32
 typedef struct MM_rq {
   void *address;
   uint32_t size;
@@ -17,17 +18,21 @@ uint64_t test_mm(uint64_t argc, char *argv[]) {
   uint32_t total;
   uint64_t max_memory;
     int testokflag = 0;
-
   if (argc != 1)
     return -1;
-
-  if ((max_memory = hexToULL(argv[0])) <= 0)
-    return -1;
-
+  int numberOfMB = satoi(argv[1]);
+    call_drawStringFormatted("Received Memory: ", BLACK, LIGHT_GRAY, 2);
+    call_printInteger(numberOfMB);
+    call_drawStringFormatted("MB\n", BLACK, LIGHT_GRAY, 2);
+    if(numberOfMB < MINIMUM_MEMORY){
+        call_drawStringFormatted("The amount of memory provided is too small!!\n", BLACK, LIGHT_GRAY, 2);
+        call_exit();
+    }
+  uint64_t aux1 =(uint64_t) numberOfMB*MEGABYTES_COEFFICIENT;
+max_memory = aux1;
   while (1) {
     rq = 0;
     total = 0;
-
     // Request as many blocks as we can
     while (rq < MAX_BLOCKS && total < max_memory) {
       mm_rqs[rq].size = GetUniform(max_memory - total - 1)/A_USEFUL_COEFFICIENT + 1;
@@ -39,10 +44,10 @@ uint64_t test_mm(uint64_t argc, char *argv[]) {
             total += mm_rqs[rq].size;
             call_printHex(mm_rqs[rq].address);
             rq++;
-            call_drawStringFormatted("\nrequest ok\n", BLACK, WHITE, 2);
+            call_drawStringFormatted("\nrequest ok\n", BLACK, LIGHT_GRAY, 2);
             call_wait(10);
         } else {
-            call_drawStringFormatted("Allocation failed - Block too large or not enough space\n", RED, WHITE, 2);
+            call_drawStringFormatted("Allocation failed - Block too large or not enough space\n", RED, LIGHT_GRAY, 2);
             //break;
         }
         call_setXBuffer(0);
@@ -51,14 +56,14 @@ uint64_t test_mm(uint64_t argc, char *argv[]) {
         call_wait(5);
     }
     // Set
-      call_drawStringFormatted("Setting memory...\n", BLACK, WHITE, 2);
+      call_drawStringFormatted("Setting memory...\n", BLACK, LIGHT_GRAY, 2);
     uint32_t i;
     for (i = 0; i < rq; i++)
       if (mm_rqs[i].address)
         memset(mm_rqs[i].address, i, mm_rqs[i].size);
 
     // Check
-      call_drawStringFormatted("Checking memory...\n", BLACK, WHITE, 2);
+      call_drawStringFormatted("Checking memory...\n", BLACK, LIGHT_GRAY, 2);
     for (i = 0; i < rq; i++)
       if (mm_rqs[i].address)
         if (!memcheck(mm_rqs[i].address, i, mm_rqs[i].size)) {
@@ -68,13 +73,13 @@ uint64_t test_mm(uint64_t argc, char *argv[]) {
         }
 
     // Free
-      call_drawStringFormatted("Freeing memory...\n", BLACK, WHITE, 2);
+      call_drawStringFormatted("Freeing memory...\n", BLACK, LIGHT_GRAY, 2);
     for (i = 0; i < rq; i++)
       if (mm_rqs[i].address)
         call_free(mm_rqs[i].address);
 
     if(!testokflag){
-        call_drawStringFormatted("MM TEST OK\n", BLACK, WHITE, 2);
+        call_drawStringFormatted("MM TEST OK\n", BLACK, LIGHT_GRAY, 2);
         testokflag = 1;
     }
       call_wait(40);
