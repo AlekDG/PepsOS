103a53cfed1c983d2864e10110b5c89779d8a0b2
Blemmingan <jyulan@itba.edu.ar>
2024-06-16 16:55:59 -0300

    aaaaa

--- Kernel/BuddyMemoryManager.c
+++ Kernel/BuddyMemoryManager.c
@@ -65,21 +65,19 @@ void *allocMemoryImpl(MemoryManagerADT manager, size_t size) {
     manager->freeLists[bsize] = block->nextBlock;
     return block->startAddress;
   }
-  else{
       BlockADT newBlock = manager->freeLists[bsize];
       newBlock->startAddress = manager->freeLists[bsize]->startAddress + sizeof(BlockCDT);
       newBlock->size = blockSize;
       newBlock->isFree = FALSE;
       newBlock->nextBlock = NULL;
       manager->spaceUsed += blockSize;
-  }
 
 
 
   while (newBlock->size > size * 2) {
     newBlock->size /= 2;
-      BlockADT buddyBlock = (BlockADT)startAddress;
-      buddyBlock->startAddress = startAddress + sizeof(BlockCDT);
+      BlockADT buddyBlock =  manager->freeLists[bsize];
+      buddyBlock->startAddress = manager->freeLists[bsize]->startAddress + sizeof(BlockCDT);
     buddyBlock->size = newBlock->size;
     buddyBlock->isFree = TRUE;
     buddyBlock->nextBlock = manager->freeLists[fast_log2(buddyBlock->size)];
