7bc695291a075584da597a011c2d006d5e9dac5c
Blemmingan <jyulan@itba.edu.ar>
2024-06-16 16:59:29 -0300

    aaaaa

--- Kernel/include/BuddyMemoryManager.h
+++ Kernel/include/BuddyMemoryManager.h
@@ -6,7 +6,7 @@
 
 #define TRUE 1
 #define FALSE 0
-
+#define POWER_OF_TWO_MAX_EXPONENT 100
 typedef struct MemoryManagerCDT *MemoryManagerADT;
 MemoryManagerADT createMemoryManagerImpl(void *const restrict memoryForMemoryManager, void *const restrict managedMemory);
 void initManagerImpl(MemoryManagerADT manager);
