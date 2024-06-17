657eb8e629f23b9b50394a6d197a6e916c5140ab
Tobias Pugliano <tpugliano@itba.edu.ar>
2024-06-17 13:24:56 -0300

    Fix warning on irqDispacher.c

--- Kernel/include/scheduler.h
+++ Kernel/include/scheduler.h
@@ -110,7 +110,7 @@ int changePriority(int pid, int priority);
 /**
  * Makes an array with the information of each process on the system.
  * @return An array cointaning the information of the procceses null terminated.
- * Must be free by calee when its no longer needed
+ * Must be free by calee when its no longer needed                           -> verificar si hago eso en ps
  */
 processInfo *getAllProcessInfo(int *count);
 
