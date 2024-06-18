de798c047be1827c8140116d763b8f253e34e4ca
Tobias Pugliano <tpugliano@itba.edu.ar>
2024-06-17 21:22:20 -0300

    Add coments for PVS-Studio use.

--- Kernel/memMan.c
+++ Kernel/memMan.c
@@ -1,3 +1,5 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #if USE_BUDDY==1
 #include <BuddyMemoryManager.h>
 #include <stddef.h>
